9f402417846810cbc80a2299a9b5e4f45765e0cc
Tino Junge <tino@plain.com>
2022-04-25 16:40:46 +0100

    Create ToDo notification

--- components/addTodoButton.tsx
+++ components/addTodoButton.tsx
@@ -1,10 +1,12 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import TextField from '@mui/material/TextField';
+import { useSnackbar } from 'notistack';
 import { ChangeEvent, useState } from 'react';
 
 import { useTodosContext } from '../context/todosContext';
@@ -12,6 +14,7 @@ import { DatePicker } from './datePicker';
 
 export function AddTodoButton() {
   const { addTodo } = useTodosContext();
+  const { enqueueSnackbar } = useSnackbar();
   const [open, setOpen] = useState(false);
   const [isFormInvalid, setIsFormInvalid] = useState(false);
   const [description, setDescription] = useState('');
@@ -35,7 +38,7 @@ export function AddTodoButton() {
     setDueDate(new Date());
   };
 
-  const handleAdd = () => {
+  const handleSubmit = () => {
     if (!description) {
       setIsFormInvalid(true);
       return;
@@ -43,36 +46,49 @@ export function AddTodoButton() {
     addTodo(description, dueDate);
     setOpen(false);
     resetForm();
+    enqueueSnackbar('Todo created!', {
+      variant: 'success',
+      persist: false,
+    });
   };
 
   return (
-    <Box>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Add
-      </Button>
-      <Dialog open={open} onClose={handleClose} maxWidth="lg">
-        <DialogTitle>Add a new ToDo</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="description"
-            label="Description"
-            type="text"
-            fullWidth
-            rows={4}
-            multiline
-            variant="standard"
-            onChange={handleDescriptionChange}
-            error={isFormInvalid}
-          />
-          <DatePicker date={dueDate} setDate={setDueDate} />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleAdd}>Add</Button>
-          <Button onClick={handleClose}>Cancel</Button>
-        </DialogActions>
-      </Dialog>
-    </Box>
+    <Container maxWidth="md">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-end',
+          minWidth: 300,
+        }}
+      >
+        <Button variant="contained" onClick={handleClickOpen}>
+          Add
+        </Button>
+        <Dialog open={open} onClose={handleClose} maxWidth="lg">
+          <DialogTitle>Add a new ToDo</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="description"
+              label="Description"
+              type="text"
+              fullWidth
+              rows={4}
+              multiline
+              variant="standard"
+              onChange={handleDescriptionChange}
+              error={isFormInvalid}
+            />
+            <DatePicker date={dueDate} setDate={setDueDate} />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleSubmit}>Add</Button>
+            <Button onClick={handleClose}>Cancel</Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
+    </Container>
   );
 }
